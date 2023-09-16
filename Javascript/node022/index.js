806c56752065c117e416c2d3264d7192e582a4f1
Zahid <72434947+Zahid40@users.noreply.github.com>
2023-09-16 09:59:35 +0530

    Delete Javascript directory

--- Javascript/node022/index.js
+++
@@ -1,9 +0,0 @@
-console.log("This is INDEX");
-const mod = require("./mod");
-// console.log(mod);
-// const avg = require("./mod");
-// console.log(avg(3,1,2,4));
-console.log(mod.avg(3,1,2,4));
-console.log(mod.name);
-console.log(mod.cartoon);
-
--- Javascript/node022/mod.js
+++
@@ -1,9 +0,0 @@
-function avg(a , b , c , d) {
-    const sum = a + b + c + d ;
-    return sum/avg.length;
-}
-module.exports = {
-    avg : avg ,
-    name : "Zahid" ,
-    cartoon : "doremon"
-}
